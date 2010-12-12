2290690411dbbd4b8fe8b4fffac2329f660eb724
Rod Hamilton <rodhamilton78@gmail.com>
2010-12-12 22:25:15 +1100

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,13 @@ class PagesController < ApplicationController
   def contact
     @title = "Contact"
   end
+  
+  def help
+    @title = "Help"
+  end
 
   def about
     @title = "About"
   end
+
 end
